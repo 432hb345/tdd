1a9a7bf027f484e3f28e8382818069235598eaf4
Marta K <martakostyk@op.pl>
2018-10-23 20:32:01 +0200

    create test for equality, then use Fake It strategy to pass test

--- src/main/java/martak/Dollar.java
+++ src/main/java/martak/Dollar.java
@@ -16,4 +16,8 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
+    public boolean equals(Object object) {
+        return true;
+    }
+
 }
--- src/test/java/martak/MultiplicationTest.java
+++ src/test/java/martak/MultiplicationTest.java
@@ -4,16 +4,23 @@ package martak;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MultiplicationTest {
 
     @Test
-        public void shouldMultiply() {
-            Dollar five = new Dollar(5);
-            Dollar product = five.multiply(2);
-            assertEquals(10, product.getAmount());
-            product = five.multiply(3);
-            assertEquals(15, product.getAmount());
-        }
+    public void shouldMultiply() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.multiply(2);
+        assertEquals(10, product.getAmount());
+        product = five.multiply(3);
+        assertEquals(15, product.getAmount());
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+    }
+
 
 }
