62947ab253b6625f474bb46dcf6accb5019f7af7
Marta K <martakostyk@op.pl>
2018-10-23 19:57:33 +0200

    do the least to compile the test

---
+++ src/main/java/martak/Dollar.java
@@ -0,0 +1,17 @@
+package martak;
+
+public class Dollar {
+
+    public Dollar(int amount) {
+    }
+
+    void multiply(int multiplier) {
+    }
+
+    int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+}
