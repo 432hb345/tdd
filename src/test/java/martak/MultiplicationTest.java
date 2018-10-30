fa3278425a1fdcdb2d58d4282bb7b2727d3b64f4
Marta K <martakostyk@op.pl>
2018-10-30 19:44:48 +0100

    compare francs to Dollars

--- src/test/java/martak/MultiplicationTest.java
+++ src/test/java/martak/MultiplicationTest.java
@@ -23,6 +23,7 @@ public class MultiplicationTest {
         assertEquals(new Dollar(15), five.multiply(3));
         assertTrue(new Franc(10).equals(new Franc(10)));
         assertFalse(new Franc(10).equals(new Franc(115)));
+        assertFalse(new Franc(10).equals(new Dollar(10)));
     }
 
     @Test
